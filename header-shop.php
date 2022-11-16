d247ade90ec75b9fe13c112b5f56318bf50e8989
nick-devv <106621471+nick-devv@users.noreply.github.com>
2022-11-16 05:12:30 -0300

    feature/new-home

--- header-shop.php
+++ header-shop.php
@@ -14,12 +14,11 @@
 			<link rel="preconnect" href="https://fonts.gstatic.com">
 			<link rel="preconnect" href="https://fonts.gstatic.com">
 			<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,700;1,400&display=swap" rel="stylesheet">
-			<link href="<?php echo get_template_directory_uri(); ?>/assets/dist/fonts/vinila.css" rel="stylesheet" />
 			<link href="<?php echo get_template_directory_uri(); ?>/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
 			<link href="<?php echo get_template_directory_uri(); ?>/assets/plugins/wow/animate.css" rel="stylesheet" />
 			<link href="<?php echo get_template_directory_uri(); ?>/assets/plugins/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
 			<link href="<?php echo get_template_directory_uri(); ?>/assets/plugins/fontawesome/css/fontawesome-all.min.css" rel="stylesheet" />
-			<link href="<?php echo get_template_directory_uri(); ?>/assets/dist/css/style.css?v=4.1" rel="stylesheet" />
+			<link href="<?php echo get_template_directory_uri(); ?>/assets/dist/css/style.css" rel="stylesheet" />
 			<!-- ================== END BASE CSS STYLE ================== -->
 
 			<!-- ================== FAVICON END ================== -->
@@ -39,40 +38,24 @@
 			<?php endif; ?>
 		</head>
 		<body <?php body_class(); ?>>
-		<header class="black">
-			<div class="container">
-				<div class="row">
+		<header>
+			<div class="container-980">
+				<div class="row justify-content-between">
 					<div class="col-md-3 col-5 logo">
-						<a href="<?php echo get_home_url();?>" class='logo'><img src="<?php echo get_template_directory_uri(); ?>/assets/img/logoheader.png"/></a>
+						<a href="<?php echo get_home_url();?>" class='logo'><img src="<?php echo get_template_directory_uri(); ?>/assets/img/new-home/logo-black.png"/></a>
 					</div>
-					<div class="col-md-6 text-center links d-none d-md-flex">
+					<div class="col-md-6 text-right links d-none d-md-flex justify-content-end">
 						<?php
 						wp_nav_menu( array( 
 							'theme_location' => 'header-menu'
 							) ); 
 						?>
 					</div>
-					<div class="col-md-3 col-7 text-right buttons">
-						<a class='btn-exp' href="#search"><i class='fal fa-search'></i></a>
-						<?php
-						if ( is_user_logged_in() ) {
-							?>
-							<a class='btn-exp' href="#myaccount"><i class='fal fa-user'></i></a>
-							<?php
-						} else {
-							?>
-							<a class='btn-exp' href="#login"><i class='fal fa-user'></i></a>
-							<?php
-						}
-						?>						
-						<a class='btn-exp' href="#cart"><i class='fal fa-shopping-cart'></i></a>
-						<a class='btn-exp d-inline-block d-md-none' href="#nav"><i class='fal fa-bars'></i></a>
-					</div>
 				</div>
 			</div>
 		</header>
 		
-		<div class="box-expansivel box-myaccount" data-name='myaccount'>
+		<!-- <div class="box-expansivel box-myaccount" data-name='myaccount'>
 			<div class='header'>
 				<h3>Minha Conta</h3>
 				<button class='bclose'><i class='fal fa-times'></i></button>
@@ -85,25 +68,9 @@
 					) ); 
 				?>
 			</div>			
-		</div>
+		</div> -->
 
-		
-		<div class="box-expansivel box-myaccount" data-name='nav'>
-			<div class='header'>
-				<h3>Menu</h3>
-				<button class='bclose'><i class='fal fa-times'></i></button>
-			</div>
-			<div class="scroll">
-				
-				<?php
-				wp_nav_menu( array( 
-					'theme_location' => 'header-menu'
-					) ); 
-				?>
-			</div>			
-		</div>
-
-		<div class="box-expansivel box-login" data-name='login'>
+		<!-- <div class="box-expansivel box-login" data-name='login'>
 			<div class='header'>
 				<h3>Área do Cliente</h3>
 				<button class='bclose'><i class='fal fa-times'></i></button>
@@ -113,9 +80,9 @@
 					<?php woocommerce_login_form( );?>
 				</div>
 			</div>
-		</div>
+		</div> -->
 
-		<div class="box-expansivel" data-name='cart'>
+		<!-- <div class="box-expansivel" data-name='cart'>
 			<div class='header'>
 				<h3>Carrinho</h3>
 				<button class='bclose'><i class='fal fa-times'></i></button>
@@ -125,10 +92,10 @@
 					<?php woocommerce_mini_cart(); ?>		
 				</div>
 			</div>
-		</div>
+		</div> -->
 
 
-		<div class="box-expansivel box-search" data-name='search'>
+		<!-- <div class="box-expansivel box-search" data-name='search'>
 			<div class='header'>
 				<h3>Busca</h3>
 				<button class='bclose'><i class='fal fa-times'></i></button>
@@ -148,4 +115,4 @@
 					</div>
 				</form>
 			</div>
-		</div>
+		</div> -->
