346d806236d82a08047f5fee88feca364df40c16
Lucas <lucasrafaelleite@gmail.com>
2023-01-04 21:15:57 -0300

    page b2b final

--- page-b2b.php
+++ page-b2b.php
@@ -9,7 +9,7 @@ get_header('shop');
          <h2>Garanta<br> retorno com<br> <strong>Investimentos<br> em T&D</strong></h2>
       </div>
       <div class="header__right">
-         <h2>E crie <strong>planos<br> de desenvolvimento<br> assertivos</strong></h2>
+         <h2>E crie <strong>planos de<br> desenvolvimento<br> assertivos</strong></h2>
       </div>
       <div class="header__cta">
          <a href="#">Entre em contato</a>
@@ -17,6 +17,8 @@ get_header('shop');
       <div class="header__last">
          <h2><strong>Aprendizado direcionado<br> ao contexto real e prático<br> de cada colaborador.</strong></h2>
       </div>
+      <img src="<?= get_template_directory_uri(); ?>/assets/img/bg-header-b2b.png" alt="" class="seta">
+      <img src="<?= get_template_directory_uri(); ?>/assets/img/bg-header-b2b-responsivo.png" alt="" class="seta mobile" style="display:none">
    </div>
 </section>
 
@@ -123,6 +125,330 @@ get_header('shop');
       </div>
    </div>
 </section>
+
+
+<section class="disc__self">
+   <div class="container principal">
+      <div class="disc__container">
+         <h2>DISC</h2>
+         <ul>
+            <li>PERFIL <br>COMPORTAMENTAL</li>
+            <li>MAPEAMENTO <br>POR ESTÍMULO</li>
+            <li>PERCEPÇÃO CLARA <br>DE AUTOAVALIAÇÃO</li>
+            <li>ALTA TENDÊNCIA <br>DE VIESES</li>
+            <li>AMBIENTE <br>TRADICIONAL</li>
+            <li>SOLUÇÕES <br>GENÉRICAS</li>
+            <li>UTILIZAÇÃO <br>LIMITADA</li>
+            <li>RETRATO ESTÁTICO DO <br>COLABORADOR. <br><strong>COLABORADOR É ASSIM.</strong></li>
+         </ul>
+      </div>
+      <div class="self__container">
+         <h2>SELF</h2>
+         <ul>
+            <li>MAPEAMENTO DE <br>DOMINÂNCIAS</li>
+            <li>MAPEAMENTO POR <br>TOMADA DE DECISÃO</li>
+            <li>ENGAJAMENTO ÁS <br>TOMADAS DE DECISÕES</li>
+            <li>BAIXA TENDÊNCIA <br>DE VIESES</li>
+            <li>AMBIENTE GAMIFICADO <br>(STORYTELLING, SIMULADOR E IA)</li>
+            <li>SOLUÇÕES CONTEXTUALIZADAS <br>(AMBIENTES DIVERSIFICADOS)</li>
+            <li>UTILIZAÇÃO AMPLA COM <br>POSSIBILIDADE DE CUSTOMIZAÇÃO</li>
+            <li>PODE-SE ADQUIRIR <br>COMPETÊNCIAS E  <br>COMPORTAMENTOS. <br><strong>COLABORADOR ESTÁ ASSIM</strong>.</li>
+         </ul>
+      </div>
+   </div>
+   
+   <div class="container">
+      <div class="disc__title">
+         <span>Mensure</span>
+         <span class="text-center">Desenvolva</span>
+         <span class="text-right">Mensure</span>
+      </div>
+
+      <div class="disc__cta">
+         <p>Nunca mais treinamentos inefetivos <br>
+         Investimentos em T&D c/ resultados<br>
+         comprováveis</p>
+         <a href="#">Entre em Contato</a>
+      </div>
+   </div>   
+</section>
+
+<section class="mensure">
+   <div class="container">
+      <h2>Mensure</h2>
+   </div>
+   <div class="content">
+      <div class="container">
+         <div class="mensure__assessments">
+            <h3>Assessments</h3>
+            <div class="mensure__assessments-content">
+               <h5>TRABALHAMOS COM 24 <br>TIPOS DE ASSESSMENTS.</h5>
+               <div class="assessments__slider">
+                  <?php for ($i = 0; $i <= 5; $i++): ?>
+                  <div class="slider__item  <?php if ($i == 0): ?>active<?php endif; ?>" id="slider__<?= $i; ?>">
+                     <div class="slider__box">
+                        <h6><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-lideranca.bmp">Liderança 4.0</h6>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry, Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially</p>
+                     </div>
+                  </div>
+                  <?php endfor; ?>
+                  <div class="slider__nav">
+                     <img src="<?= get_template_directory_uri(); ?>/assets/img/next-slider.bmp">
+                     <img src="<?= get_template_directory_uri(); ?>/assets/img/prev-slider.bmp">
+                  </div>
+                  <div class="slider__dots">
+                     <?php for ($i = 0; $i <= 5; $i++): ?>
+                        <div class="dot <?php if ($i == 0): ?>active<?php endif; ?>" id="slider__<?= $i; ?>"></div>
+                     <?php endfor; ?>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="competencias">
+            <h3>COMPETÊNCIAS</h3>
+
+            <ul class="competencias__list">
+               <li class="active">
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Adaptabilidade</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Autoconhecimento</p>   
+               </li>
+               <li  class="active">
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-interatividade.bmp" alt=""></div>
+                  <p>Interatividade</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Inovação e Empreendedorismo</p>   
+               </li>
+               <li class="active">
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-empatia.bmp" alt=""></div>
+                  <p>Empatia</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Conectividade</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Solucionar Problemas</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Afetividade</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Autodomínio</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Mindset Crítico e Digital</p>   
+               </li>
+               <li class="active">
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-relacionamento.bmp" alt=""></div>
+                  <p>Relacionamento Conectivo</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Trabalho em Equipe</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Resiliência</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Relacionamento Interpessoal</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Liderança</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Motivação</p>   
+               </li>
+               <li class="active">
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-mindset.bmp" alt=""></div>
+                  <p>Mindset Inovador</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-mindset.bmp" alt=""></div>
+                  <p>Geração de Valor</p>   
+               </li>
+               <li class="active">
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Solucionar Problemas</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Simpatia</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Percepção</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Produtividade</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Adaptabilidade</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Proatividade</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Inteligência Emocional</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Visão Sistemica</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Aceitação de Riscos</p>   
+               </li>
+               <li>
+                  <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-adaptabilidade.bmp" alt=""></div>
+                  <p>Influência</p>   
+               </li>
+            </ul>
+         </div>
+
+         <div class="analise">
+            <h5>ENTENDA AS FORÇAS EFRAQUEZASDE <br>CADA COLABORADOR E TIME:</h5>
+
+            <h3>Análise</h3>
+            <img src="<?= get_template_directory_uri(); ?>/assets/img/analise.bmp" alt="">
+
+            <div class="disclaimer">
+               <p>Time</p>
+               <p>Colaborador</p>
+            </div>
+         </div>
+
+      </div>
+   </div>
+</section>
+
+
+<section class="desenvolva">
+   <div class="container">
+      <h2>Desenvolva</h2>
+   </div>
+   <div class="content">
+      <div class="container">
+         <h5>INDICAMOS OS PROGRAMAS DE APRENDIZADO<br> QUE DESENVOLVEM COMPETÊNCIAS QUE<br> CADA COLABORADOR PRECISA,</h5>
+
+         <div class="desenvolva__boxes">
+            <div class="left">
+               <h3>Competência</h3>
+               <div class="box" style="background-image:url(https://saibala.com.br/wp-content/uploads/2021/04/PROGRAMA_ProcessoCriativo_HERO.jpg)">
+                  <div class="content">
+                     <div class="icone"><img src="<?= get_template_directory_uri(); ?>/assets/img/icone-empatia-analise.bmp" alt=""></div>
+                     <div class="texto">
+                        <h6>Empatia</h6>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda laudantium iure et magni nam.  adipisicing elit. Assumenda laudantium iure et magni nam  adipisicing elit.</p>
+                     </div>
+                  </div>
+               </div>
+            </div>
+            <img src="<?= get_template_directory_uri(); ?>/assets/img/seta-desenvolva.bmp" alt="">
+            <div class="right">
+               <h3>PROGRAMA <br>SELECIONADO</h3>
+               <div class="box" style="background-image:url(https://saibala.com.br/wp-content/uploads/2022/07/HERO_TecMudarmundo.jpg)">
+                  <div class="content">
+                     <div class="texto">
+                        <h6>Programa de Comunicação não violenta</h6>
+                        <p class="autor">Evarisco Costa</p>
+                        <a href="#">Ver mais</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="mensure__two">
+   <div class="container">
+      <h2>Mensure</h2>
+   </div>
+   <div class="content">
+      <div class="container">
+         <div class="intro">
+            <h4>APLICAMOS MAIS UM ASSESSMENT,<br> PARA GARANTIR DE FORMA ESTATÍSTICA<br> EFETIVIDADE DO PROCESSO DE<br> APRENDIZAGEM DE CADA UM.</h4>
+            <img src="<?= get_template_directory_uri(); ?>/assets/img/seta-mensure.bmp" alt="">
+         </div>     
+      </div>
+   </div>
+   <div class="number azul">
+      <h3>1</h3>
+      <p>ALINHAMENTO DOS<br> T&D COM OBJETIVOS<br> CORPORATIVOS</p>
+   </div>
+   <div class="number roxo">
+      <h3>2</h3>
+      <p>CONHÇA SEU TIME DE <br>FORMA INDIVIDUALIZADA,<br> QUANTITATIVA E AINDA<br> ASSIM ESCALÁVEL</p>
+   </div>
+   <div class="number amarelo">
+      <h3>3</h3>
+      <p>COMPARE AS<br> COMPETÊNCIAS DO<br> SEU TIME FRENTE <br>AO MERCADO</p>
+   </div>
+</section>
+
+<section class="cta">
+   <div class="container">
+      <div class="box__cta">
+         <div class="content">
+            <h2>TORNE SEU TIME O MAIS <br>PREPARADO DO MERCADO</h2>
+            <a href="#">Faça uma consulta</a>
+         </div>
+      </div>
+   </div>
+</section>
+
+
+<section class="clientes">
+   <div class="container">
+      <h2>EMPRESAS QUE ESTÃO CONOSCO NESSA REVOLUÇÃO:</h2>
+      <ul>
+         <?php for ($i = 1; $i <= 21; $i++): ?>
+            <li <?php if ($i >= 7): echo 'class="demais" style="display:none"'; endif; ?>><img src="<?= get_template_directory_uri(); ?>/assets/img/cliente-<?= $i; ?>.png" alt=""></li>
+         <?php endfor; ?>
+      </ul>
+      <a href="#"><span>ver mais</span> <img src="<?= get_template_directory_uri(); ?>/assets/img/seta-vermais-clientes.bmp" alt=""></a>
+   </div>
+</section>
+
+
+<script>
+jQuery.noConflict();
+   (function( $ ) {
+   $(function() {
+      $('section.clientes a').click(function(event) {
+         event.preventDefault();
+         console.log($(this).text());
+         if ($(this).find('span').text() == 'ver mais') {
+            $(this).addClass('active').find('span').text('ver menos');
+            $('section.clientes ul li.demais').fadeIn();
+         } else {
+            $(this).removeClass('active').find('span').text('ver mais');
+            $('section.clientes ul li.demais').fadeOut();
+         }
+      })
+   });
+})(jQuery);
+</script>
 
 <?php
 get_footer();
