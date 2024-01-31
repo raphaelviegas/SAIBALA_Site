dc20948ede62ce6a3f96ec870d64de7e5317dfa4
Lucas Rafael Leite <lucasrafaelleite@gmail.com>
2024-01-30 22:13:41 -0300

    add fields header background

--- functions.php
+++ functions.php
@@ -886,4 +886,70 @@ function woocommerce_custom_update_checkout() {
   });
 
 </script>
-<?php }} ?>
+<?php }}
+
+
+
+
+
+
+if( function_exists('acf_add_local_field_group') ):
+
+  acf_add_local_field_group(array(
+    'key' => 'group_65b99c2f66895',
+    'title' => 'Cursos - Introdução',
+    'fields' => array(
+      array(
+        'key' => 'field_65b99c41d02ca',
+        'label' => 'Background Introdução',
+        'name' => 'background_introducao',
+        'type' => 'image',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'return_format' => 'url',
+        'preview_size' => 'thumbnail',
+        'library' => 'all',
+        'min_width' => '',
+        'min_height' => '',
+        'min_size' => '',
+        'max_width' => '',
+        'max_height' => '',
+        'max_size' => '',
+        'mime_types' => '',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'product',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+  ));
+  
+  endif;
+
+
+
+
+
+
+
+
+?>
