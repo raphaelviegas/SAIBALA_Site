1fbf2aa3e54791a43facfe8a0e44159a8ff357df
Lucas Rafael Leite <lucasrafaelleite@gmail.com>
2024-03-05 21:18:22 -0300

    change values courses [promotional]

--- functions.php
+++ functions.php
@@ -942,6 +942,50 @@ if( function_exists('acf_add_local_field_group') ):
     'active' => true,
     'description' => '',
   ));
+
+
+  acf_add_local_field_group(array(
+    'key' => 'group_65e7b3bb06af5',
+    'title' => 'Exibição de Valores',
+    'fields' => array(
+      array(
+        'key' => 'field_65e7b3c7f585a',
+        'label' => 'Ocultar Desconto',
+        'name' => 'ocultar_desconto',
+        'type' => 'true_false',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'message' => 'Marque para ocultar o desconto do produto.',
+        'default_value' => 0,
+        'ui' => 0,
+        'ui_on_text' => '',
+        'ui_off_text' => '',
+      ),
+    ),
+    'location' => array(
+      array(
+        array(
+          'param' => 'post_type',
+          'operator' => '==',
+          'value' => 'product',
+        ),
+      ),
+    ),
+    'menu_order' => 0,
+    'position' => 'normal',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+  ));
   
   endif;
 
